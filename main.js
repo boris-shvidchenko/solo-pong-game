2c64e081f4a015ff0c9fefc82e0c500fb7978581
Boris Shvidchenko <101944376+boris-shvidchenko@users.noreply.github.com>
2022-07-01 14:37:57 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,13 @@
 const gameArea = document.getElementById("gameArea");
 const startGameButton = document.getElementById("startGameButton");
 const messageArea = document.getElementById("messageArea");
+const gameInfo = document.getElementById("gameInfo");
 const playerPad = document.getElementById("playerPad");
 const ballElem = document.getElementById("ball");
 const score = document.getElementById("score");
+const scoreText = document.getElementById("scoreText");
 
-// Gameplay objects
+// Gameplay objects and variables
 let controls = {
     ArrowLeft: false,
     ArrowRight: false
@@ -24,6 +26,9 @@ let ball = {
     ballY: 696,
 };
 
+let moveBall = true;
+let gameScore = 0;
+
 // Event Listeners
 startGameButton.addEventListener("click", startGame);
 document.addEventListener("keydown", pressOn);
@@ -31,13 +36,17 @@ document.addEventListener("keyup", pressOff);
 
 // Functions
 function startGame() {
+    console.log("game started");
     messageArea.setAttribute("class", "remove"); // remove message area
+    console.log("remove message area");
     score.removeAttribute("class", "remove");    // add score area
+    console.log("score removed");
     player.start = true;
+    console.log(player.start);
     player.score = 0;
+    console.log(player.score);
     window.requestAnimationFrame(playGame);
     playGame();
-
 }
 
 function playGame() {
@@ -48,21 +57,39 @@ function playGame() {
         playerPad.style.left = player.x + "px";
         window.requestAnimationFrame(playGame);
     }
-
 }
 
 function ballMoves() {
-    // ball.ballY -= ball.ballSpeed;
-    // ballElem.style.top = ball.ballY + "px";
-
-    if (ball.ballY >= -1) {
-        ball.ballY -= ball.ballSpeed;
-        ballElem.style.top = ball.ballY + "px"; 
-    } else if (ball.ballY === -1){
-        console.log("true");
-        ball.ballY += ball.ballSpeed;
-        ballElem.style.top = ball.ballY + "px"; 
+    if (moveBall) {
+        if (ball.ballY > -1) {
+            ball.ballY -= ball.ballSpeed;
+            ballElem.style.top = ball.ballY + "px"; 
+        } else {
+            moveBall = false;
+        }
+    } else {
+        if (ballHit(playerPad,ballElem)) {
+            scoreText.innerText = ++gameScore;
+            moveBall = true;
+        }         
+        else if (ball.ballY > 725) {
+            endGame();
+        } else {
+            ball.ballY += ball.ballSpeed;
+            ballElem.style.top = ball.ballY + "px"; 
+        }      
     }
+}
+
+function ballHit(a,b) {
+    let playerPadDimension = a.getBoundingClientRect();
+    let ballElemDimension = b.getBoundingClientRect();
+    return !(
+        (playerPadDimension.bottom < ballElemDimension.top) ||
+        (playerPadDimension.top > ballElemDimension.bottom) ||
+        (playerPadDimension.right < ballElemDimension.left) ||
+        (playerPadDimension.left > ballElemDimension.right)
+    );
 }
 
 function pressOn(e) {
@@ -74,3 +101,15 @@ function pressOff(e) {
     e.preventDefault();
     controls[e.key] = false;
 }
+
+function endGame() {
+    player.start = false;
+    messageArea.removeAttribute("class", "remove"); // add message area
+    gameInfo.innerText = `You lost! You scored ${gameScore} points.`;
+    startGameButton.style.display = "none";
+    setTimeout(() => {          // after 3 seconds, reload game/page
+        location.reload()
+    }, 3000);
+}
+
+
